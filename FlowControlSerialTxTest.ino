39b8d12a208e4f5d4c474a5f1d04fbfb72d14074
jim11662418 <51113579+jim11662418@users.noreply.github.com>
2019-06-25 09:37:03 -0400

    Add files via upload

---
+++ FlowControlSerialTxTest.ino
@@ -0,0 +1,40 @@
+/*
+ Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX input is  pin 8   - goes to TX of connected device)
+ * TX output is  pin 9  - goes to RX of connected device)
+ * RTS output is pin 11 - goes to CTS of connected device)
+ * CTS input is pin 12  - goes to RTS of connected device
+
+  Not all pins on the Leonardo and Micro support change interrupts,
+  so only the following can be used for RX: 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+*/
+
+#include "FlowControlSoftSerial.h"
+
+FlowControlSoftSerial softSerial;
+uint8_t ledPin = 13;
+
+void setup() {
+
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,LOW);
+  
+  
+  // open software serial communications port
+  softSerial.begin(9600);     // although tested at 19200 bps, 9600 bps seems to work best
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() { // run over and over
+   softSerial.println("Hello, software serial communications port!");
+   Serial.println("Hello, software serial communications port!");     
+   delay(10);
+
+}
+
---
+++ FlowControlSoftSerial.h
@@ -0,0 +1,66 @@
+/* An Alternative Software Serial Library
+ * http://www.pjrc.com/teensy/td_libs_FlowControlSoftSerial.html
+ * Copyright (c) 2014 PJRC.COM, LLC, Paul Stoffregen, paul@pjrc.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// Revised to incorporate RTS/CTS hardware flow control. Jim Loos - 03/16/2018
+
+#ifndef FlowControlSoftSerial_h
+#define FlowControlSoftSerial_h
+
+#include <inttypes.h>
+#include "Arduino.h"
+
+#define ALTSS_BASE_FREQ F_CPU
+
+class FlowControlSoftSerial : public Stream {
+public:
+	FlowControlSoftSerial() { }
+	~FlowControlSoftSerial() { end(); }
+	static void begin(uint32_t baud) { init((ALTSS_BASE_FREQ + baud / 2) / baud); }
+	static void end();
+	int peek();
+	int read();
+	int available();
+#if ARDUINO >= 100
+	size_t write(uint8_t byte) { writeByte(byte); return 1; }
+	void flush() { flushOutput(); }
+#else
+	void write(uint8_t byte) { writeByte(byte); }
+	void flush() { flushInput(); }
+#endif
+	using Print::write;
+	static void flushInput();
+	static void flushOutput();
+	// for drop-in compatibility with NewSoftSerial, rxPin & txPin ignored
+	FlowControlSoftSerial(uint8_t rxPin, uint8_t txPin, bool inverse = false) { }
+	bool listen() { return false; }
+	bool isListening() { return true; }
+	bool overflow() { bool r = timing_error; timing_error = false; return r; }
+	static int library_version() { return 1; }
+	static void enable_timer0(bool enable) { }
+	static bool timing_error;
+private:
+	static void init(uint32_t cycles_per_bit);
+	static void writeByte(uint8_t byte);
+};
+
+#endif
